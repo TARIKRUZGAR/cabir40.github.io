5fed8e5809d2b56c52e2dde7a0a2a1723f0097be
cabir <64752006+Cabir40@users.noreply.github.com>
2020-05-03 23:42:31 +0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+alert('MATH-101 e Hoşgeldiniz')
+
+
+$(document).ready(function(){
+    $("#hamburger").click(function(){
+        $(".sidebar-left").toggle();
+
+
+        document.querySelector('#orta').className='col-md-8'
+        
+        
+    })
+})
